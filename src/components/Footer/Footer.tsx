a19f0e731f4bcd7f668d8deb01bb89432d970a73
Joash-star <care@nopovo.com>
2020-02-02 04:46:48 +0300

    king

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -3,46 +3,19 @@ import "./scss/index.scss";
 import * as React from "react";
 
 import { SocialMediaIcon } from "..";
-import { SOCIAL_MEDIA} from "../../core/config";
+import { SOCIAL_MEDIA } from "../../core/config";
 import Nav from "./Nav";
-import abc from "../../images/abc.svg";
-import america from "../../images/america.svg";
-import bcd from "../../images/bcd.svg";
-import cici from "../../images/cici.svg";
-import jcb from "../../images/jcb.svg";
-import maestro from "../../images/maestro.svg";
-import master from "../../images/master.svg";
-import visa from "../../images/visa.svg";
 
 
 const Footer: React.FC = () => (
   <div className="footer" id="footer">
-  <div className="pay juu">FOLLOW US</div>
     <div className="footer__favicons container">
       {SOCIAL_MEDIA.map(medium => (
-        <SocialMediaIcon medium={medium} key={medium.ariaLabel}  />
+        <SocialMediaIcon medium={medium} key={medium.ariaLabel} />
       ))}
     </div>
     <Nav />
-    <div className="pay">PAYMENT METHOD</div>
-     <div className=" container tra" >
-     <img src={america}  height="28" />
-     <img src={visa}  height="28" />
-   <img src={master}  height="28" />
-   <img src={maestro}  height="28" />
-<img src={bcd}  height="28" />
-   <img src={jcb} height="28" /> 
-    <img src={abc}  height="28" />
-   <img src={cici}  height="28" />
-   
-  
-   </div>
   </div>
 );
 
 export default Footer;
-
-
-
-
- 
